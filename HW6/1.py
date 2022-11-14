a67722995d1cc3698e91ea9f4e550cde4166ab23
OlegVladimirovichG <hankat@mail.ru>
2022-11-14 20:48:25 +0300

    решил перву задачу

--- HW6/1.py
+++ HW6/1.py
@@ -1,46 +1,7 @@
-from decimal import Decimal
+'''Напишите программу вычисления арифметического
+выражения заданного строкой. Используйте операции
++,-,/,*. приоритет операций стандартный.'''
 
-def create_list(expr):
-    num = ''
-    op = ''
+st_ring = '2+2*2'
+print(eval(st_ring))
 
-    for ch in expr:
-        if ch.isdigit() or ch == '.':
-            num += ch
-        elif ch == '-':
-            num += ' ' + ch
-            op += '+'
-        elif ch == ' ':
-            continue
-        else:
-            op += ch
-            num += ' '
-
-    num, op = list(map(Decimal, num.split())), list(op)
-
-    def make_operation(sign: str) -> None:
-        while sign in op:
-            sign_index = op.index(sign)
-            first_num = num[sign_index]
-            second_num = num[sign_index + 1]
-            if sign == '/':
-                if second_num == 0:
-                    print('Делите на 0')
-                    exit()
-                num[sign_index] = first_num / second_num
-            elif sign == '*':
-                num[sign_index] = first_num * second_num
-
-            else:
-                num[sign_index] = first_num + second_num
-
-
-            del num[sign_index + 1]
-            del op[sign_index]
-
-    for i in '/*+':
-        make_operation(i)
-
-            return num[sign_index]
-expression = '1+2*3'
-print(create_list(expression))
